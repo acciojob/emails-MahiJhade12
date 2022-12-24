3d723793ea1a75278391af4d750df9fd23710d1e
mahima <mahimajhade123@gmail.com>
2022-12-25 00:08:34 +0530

    m

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -3,9 +3,7 @@ package com.driver;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.*;
 
 public class Workspace extends Gmail{
 
@@ -34,14 +32,35 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-        HashSet<Meeting> findMax=new HashSet<>();
-        for(Meeting m:calendar){
-           // LocalTime startTime=m.startTime;
-            //if()
-            findMax.add(m);
-        }
-        int maxMeeting=findMax.size();
-        return maxMeeting;
+        Collections.sort(calendar, new Comparator<Meeting>() {
+            @Override
+            public int compare(Meeting o1, Meeting o2) {
+                if((o1.getStartTime()).compareTo(o2.getStartTime())!=0)
+                    return  (o1.getStartTime()).compareTo(o2.getStartTime());
 
+                return  (o2.getEndTime()).compareTo(o1.getEndTime());
+            }
+        });
+
+//        for( Meeting m:calendar){
+//            System.out.println(m.getStartTime()+" "+m.getEndTime());
+//        }
+        int x=1;
+        int ans=0;
+        Meeting m=calendar.get(0);
+        for(int i=1;i<calendar.size();i++){
+            if((calendar.get(i)).getStartTime().compareTo(m.getEndTime())>0){
+                x++;
+                m=calendar.get(i);
+            }
+            else{
+                ans=Math.max(ans,x);
+                m=calendar.get(i);
+                x=1;
+            }
+            ans=Math.max(ans,x);
+        }
+
+        return ans;
     }
 }
